dc693abe1adfd679701381eec5e2649009f45a83
yuzhong <qiuchangsheng@yunmai365.com>
2021-06-11 15:29:58 +0800

    微服务

---
+++ cloud-content/src/main/java/com/cloud/content/CloudContentApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.content;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudContentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudContentApplication.class, args);
+    }
+
+}
---
+++ cloud-content/src/test/java/com/cloud/content/CloudContentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.content;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudContentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
